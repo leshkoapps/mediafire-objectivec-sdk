8223e32f4c6ef8a5e073c53a79b649fa638cea4f
Daniel Dean <daniel.dean@mediafire.com>
2014-09-24 14:19:17 -0500

    update parallel lifespan

--- MediaFireSDK/MediaFireSDK/requestmanagers/MFParallelRequestManager.m
+++ MediaFireSDK/MediaFireSDK/requestmanagers/MFParallelRequestManager.m
@@ -197,7 +197,7 @@ const char* MF_PARALLEL_REQUEST_DISPATCH_QUEUE = "com.mediafire.api.req.parallel
           [asyncSelf stopWaitingForToken];
           callbacks.onerror(response);
       }};
-    [self.actionAPI getActionToken:@{@"type" : self.type, @"lifespan" : @"180"} callbacks:tokenCallbacks];
+    [self.actionAPI getActionToken:@{@"type" : self.type, @"lifespan" : @"240"} callbacks:tokenCallbacks];
 }
 
 
