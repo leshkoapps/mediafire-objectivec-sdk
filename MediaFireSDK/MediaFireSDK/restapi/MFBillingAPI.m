08611a52f8b91be776fa4644e41afd9f8a0334ec
Daniel Dean <daniel.dean@mediafire.com>
2015-09-08 17:09:16 -0500

    Include all billing APIs and update docs.

--- MediaFireSDK/MediaFireSDK/restapi/MFBillingAPI.m
+++ MediaFireSDK/MediaFireSDK/restapi/MFBillingAPI.m
@@ -32,11 +32,77 @@
 }
 
 //------------------------------------------------------------------------------
-- (void)purchasePlan:(NSDictionary*)parameters withCallbacks:(NSDictionary*)cb {
-    [self purchasePlan:@{} query:parameters withCallbacks:cb];
+- (void)cancelPlan:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self cancelPlan:@{} query:parameters callbacks:cb];
 }
 
-- (void)purchasePlan:(NSDictionary*)options query:(NSDictionary*)parameters withCallbacks:(NSDictionary*)cb {
+- (void)cancelPlan:(NSDictionary*)options query:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self createRequest:[options merge:@{HURL : @"cancel_plan.php",
+                                         HSECURE : @"true",
+                                         HMETHOD : @"POST"}]
+                  query:parameters
+              callbacks:cb];
+}
+
+//------------------------------------------------------------------------------
+- (void)changePlan:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self changePlan:@{} query:parameters callbacks:cb];
+}
+
+- (void)changePlan:(NSDictionary*)options query:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self createRequest:[options merge:@{HURL : @"change_plan.php",
+                                         HSECURE : @"true",
+                                         HMETHOD : @"POST"}]
+                  query:parameters
+              callbacks:cb];
+}
+
+//------------------------------------------------------------------------------
+- (void)getInvoice:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self getInvoice:@{} query:parameters callbacks:cb];
+}
+
+- (void)getInvoice:(NSDictionary*)options query:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self createRequest:[options merge:@{HURL : @"get_invoice.php",
+                                         HSECURE : @"true",
+                                         HMETHOD : @"POST"}]
+                  query:parameters
+              callbacks:cb];
+}
+
+//------------------------------------------------------------------------------
+- (void)getPlans:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self getPlans:@{} query:parameters callbacks:cb];
+}
+
+- (void)getPlans:(NSDictionary*)options query:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self createRequest:[options merge:@{HURL : @"get_plans.php",
+                                         HSECURE : @"true",
+                                         HMETHOD : @"POST"}]
+                  query:parameters
+              callbacks:cb];
+}
+
+//------------------------------------------------------------------------------
+- (void)getProducts:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self getProducts:@{} query:parameters callbacks:cb];
+}
+
+- (void)getProducts:(NSDictionary*)options query:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self createRequest:[options merge:@{HURL : @"get_products.php",
+                                         HSECURE : @"true",
+                                         HMETHOD : @"POST"}]
+                  query:parameters
+              callbacks:cb];
+}
+
+
+//------------------------------------------------------------------------------
+- (void)purchasePlan:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
+    [self purchasePlan:@{} query:parameters callbacks:cb];
+}
+
+- (void)purchasePlan:(NSDictionary*)options query:(NSDictionary*)parameters callbacks:(NSDictionary*)cb {
     [self createRequest:[options merge:@{HURL : @"purchase_plan.php",
                                          HSECURE : @"true",
                                          HMETHOD : @"POST"}]
